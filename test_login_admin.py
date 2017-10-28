2ae2a1aaa2e7ca8b6603fb4fdda6172f98f40499
mari44ka <marikostenko83@gmail.com>
2017-10-27 23:21:38 -0700

    login as an admin for litecart

---
+++ test_login_admin.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+
+
+
+@pytest.fixture
+def driver(request):
+    #wd = webdriver.Chrome("/Users/Mari/Downloads/chromedriver")
+    wd = webdriver.Firefox()
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_login_admin(driver):
+    driver.get("http://localhost/litecart/admin/login.php")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
